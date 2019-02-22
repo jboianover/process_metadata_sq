54da6343d8ff34ba0b04aa40a06203de9862ede6
jboianover <boia.jonathan@gmail.com>
2019-02-22 17:25:19 -0300

    Merge branch 'jboianover' of https://github.com/maxbloise/process_metadata into jboianover
    # Conflicts:
    #	bin/functions.py

--- bin/functions.py
+++ bin/functions.py
@@ -31,130 +31,183 @@ def parse_arguments(arguments):
     return process_name, chdir, file_name, output_file
 
 
-def header(file, process_table):
-    file.writelines(''.join(['insert into ', process_table, '\n']))
-    return 0
+def header(process_table):
+
+    header_query = (''.join(['insert into ', process_table, '\n']))
+
+    return header_query
 
 
-def select_clause(file, tables):
-    file.writelines(''.join(['select', '\n']))
-    tables = tables.reset_index(drop=True)
-    n_rows = len(tables.index)
+def select_clause(tables):
+    select = (''.join(['select', '\n']))
+    tables = tables.fillna('')
+    tables = tables.loc[(tables['column_name'] != '')].reset_index(drop=True)
+    n_rows = len(tables)
 
+    tables = tables.fillna('')
+    i = 0
     for index, row in tables.iterrows():
-        if row['custom_column_flag'] == 'Y':
-            file.writelines(''.join([row['column']]))
-            if index != n_rows - 1:
-                file.writelines(''.join([',', '\n']))
-        elif pd.isna(row['agg_operation']):
-            #   file.writelines(''.join([row['column_source'], '.', row['column']]))
-
-            file.writelines(''.join(['case when ', str(row['column_source']), '.', str(row['column']), ' ',
-                                     'is not null then', ' ', str(row['column_source']), '.', str(row['column']), ' ', 'else',
-                                     ' ',
-                                     str(row['default_value']), ' end']))
-            if not pd.isna(row['column_alias']):
-                file.writelines(''.join([' ', str(row['column_alias'])]))
-            if index != n_rows - 1:
-                file.writelines(''.join([',', '\n']))
-
-        else:
-            #   file.writelines(''.join([row['agg_operation'], '(', row['column_source'], '.', row['column'], ')']))
-
-            file.writelines(''.join([row['agg_operation'], '(', 'case when ', str(row['column_source']), '.', str(row['column']), ' ',
-                                     'is not null then', ' ', str(row['column_source']), '.', str(row['column']), ' ', 'else',
-                                     ' ',
-                                     str(row['default_value']), ' end', ')']))
-            if not pd.isna(row['column_alias']):
-                file.writelines(''.join([' ', row['column_alias']]))
-            if index != n_rows - 1:
-                file.writelines(''.join([',', '\n']))
-    return 0
-
-
-def from_clause(file, tables):
-    fc = tables[['source_table']].drop_duplicates().dropna().reset_index(drop=True)
-    file.writelines(''.join([' ', '\n', 'from ', fc.loc[0, 'source_table'], '\n']))
-    seqs = tables['seq'].drop_duplicates().astype(int).tolist()
-    for seq in seqs:
-        from_columns = tables.loc[(tables['seq'] == seq)].reset_index(drop=True)
-        tables = tables.reset_index(drop=True)
-        if not pd.isna(tables.loc[0, 'dim_table']):
-            file.writelines(''.join([' ', str(from_columns.loc[0, 'how']), ' join ', str(from_columns.loc[0, 'dim_table'])
-                                     , ' on ']))
-            join_columns = from_columns[['fk', 'key', 'criteria']].reset_index(drop=True).dropna()
-            n_rows = len(join_columns.index)
-            for index, row in join_columns.iterrows():
-                file.writelines(''.join([' (', row['fk'], row['criteria'], row['key'], ')']))
-                if index != n_rows - 1:
-                    file.writelines(''.join([' and ']))
-                else:
-                    file.writelines('\n')
-    return 0
-
-
-def where_clause(file, process_metadata):
-
-    dim_columns = process_metadata[['column_source', 'where_column', 'where_criteria', 'where_value', 'where_custom_flag']] \
-        .drop_duplicates().reset_index(drop=True)
-
-    dim_tables_filtered = dim_columns.loc[(dim_columns['where_column'].notnull() | dim_columns['where_value'].notnull())]
-    max_lines = len(dim_tables_filtered)
-    file.writelines('where \n')
-
-    max_lines = len(dim_tables_filtered)
-
-    i = 1
-
-    for index, row in dim_tables_filtered.iterrows():
-        if pd.isnull(row['where_value']):
-            where_value = ''
-        else:
-            where_value = (''.join([' ', str(row['where_value'])]))
-
-        if row['where_custom_flag'] == 'N':
-
-            file.writelines(''.join([row['column_source'], '.', row['where_column'], ' ', row['where_criteria'], where_value]))
-        else:
-            file.writelines(''.join([where_value, '\n']))
-
-        if i < max_lines:
-            file.writelines(' and\n')
-        else:
-            file.writelines('\n')
-        i += 1
-
-    return 0
-
-
-def groupby_clause(file, process_metadata):
-
-    dim_columns = process_metadata[['seq', 'column_source', 'column', 'custom_column_flag', 'default_value', 'agg_flg']] \
-        .drop_duplicates().reset_index(drop=True)
-    dim_tables_filtered = dim_columns.loc[(dim_columns['agg_flg'] == 'Y') & (dim_columns['column'].notnull())] \
-        .reset_index(drop=True)
-    cant_columnas_group = len(dim_tables_filtered)
-    file.writelines(''.join(['group by', '\n']))
-
-    i = 1
-
-    for index, row in dim_tables_filtered.iterrows():
-
-        if row['custom_column_flag'] == 'N':
-            if pd.isna(dim_tables_filtered.loc[index]['default_value']):
-                file.writelines(''.join([row['column_source'], '.', row['column']]))
+        if row['agg_flg'] != 'N':
+            if row['column_custom_flg'] == 'Y':
+                select += (''.join([row['column_name']]))
             else:
-                file.writelines(''.join(['case when ', row['column_source'], '.', row['column'], ' ', \
-                                'is not null then', ' ', row['column_source'], '.', row['column'], ' ', 'else', ' ', \
-                                         str(row['default_value']), ' end']))
+                select += (''.join([row['column_source'], '.', row['column_name']]))
+            if not pd.isna(row['column_alias']):
+                select += (''.join([' ', str(row['column_alias'])]))
         else:
-            file.writelines(''.join([row['column']]))
+            if row['column_custom_flg'] == 'Y':
+                select += (''.join([row['agg_operation'], '(', row['column_name'], ')']))
+            else:
+                select += (''.join([row['agg_operation'], '(', row['column_source'], '.', row['column_name'], ') ',
+                                    row['column_alias']]))
 
-        if i < cant_columnas_group:
-            file.writelines(''.join([',', '\n']))
+        if i != n_rows - 1:
+            select += (''.join([',', '\n']))
         else:
-            file.writelines(';\n')
+            select += '\n'
+        i += 1
+    return select
 
+
+def from_clause(process_metadata, dict_sq):
+    process_metadata = process_metadata.fillna('')
+    table_a_b = process_metadata[['table_a', 'table_b']].drop_duplicates().reset_index(drop=True)
+
+    if process_metadata.loc[0, 'sq_from_flg_a'] != 'Y':
+        from_sq = (''.join(['', 'from ', process_metadata.loc[0, 'table_a'], '\n']))
+    else:
+        from_sq = (''.join(['', 'from (', dict_sq[process_metadata.loc[0, 'table_a']], ')\n']))
+
+    table_b_aux = ''
+
+    for index, row in table_a_b.iterrows():
+
+        from_columns = process_metadata.loc[((process_metadata['table_a'] == row['table_a']) &
+                                            (process_metadata['table_b'] == row['table_b']))].reset_index(drop=True)
+
+        join_columns = from_columns[['sq_from_flg_a', 'table_a', 'sq_from_flg_b', 'table_b', 'how', 'fk',
+                                     'criteria', 'key']].dropna().reset_index(drop=True)
+
+        if table_b_aux != row['table_b']:
+            if join_columns.loc[0, 'sq_from_flg_b'] == 'N':
+                from_sq += (''.join(['', str(join_columns.loc[0, 'how']), ' join ',
+                                     str(join_columns.loc[0, 'table_b']), ' on\n']))
+            else:
+                from_sq += (''.join(['', str(join_columns['how']), ' join (', str(dict_sq[table_a_b.loc[0, 'table_b']]),
+                                     ') on\n']))
+        table_b_aux = row['table_b']
+
+        for index_i, row_i in join_columns.iterrows():
+
+            from_sq += (''.join(['(', row_i['table_a'], '.', row_i['fk'], ' ', row_i['criteria'], ' ',
+                                 row_i['table_b'], '.', row_i['key'], ')']))
+            if (index_i != (len(join_columns)-1)) and (table_b_aux == row_i['table_b']):
+                from_sq += ' and\n'
+            else:
+                from_sq += '\n'
+    return from_sq
+
+
+def where_clause(process_metadata, dict_sq):
+
+    dim_columns = process_metadata[['sq_where_flag_a', 'where_value_a', 'where_criteria', 'sq_where_flag_b',
+                                    'where_value_b']].drop_duplicates().reset_index(drop=True)
+
+    where_query = 'where\n'
+
+    max_lines = len(dim_columns)
+
+    i = 0
+
+    for index, row in dim_columns.iterrows():
+        if row['sq_where_flag_a'] == 'N':
+            if not pd.isnull(row['where_value_a']):
+                where_query += (''.join([str(row['where_value_a']), ' ']))
+        else:
+            where_query += (''.join([str(dict_sq[row['where_value_a']]), ' ']))
+
+        if not pd.isnull(row['where_criteria']):
+            where_query += row['where_criteria']
+        else:
+            return where_query
+
+        if row['sq_where_flag_b'] == 'N':
+            if not pd.isnull(row['where_value_b']):
+                where_query += (''.join([' ', str(row['where_value_b']), ' ']))
+        else:
+            where_query += ''.join([' (', str(dict_sq[row['where_value_b']]), ')'])
+
+        if i != max_lines - 1:
+            where_query += ' and\n'
+        else:
+            where_query += '\n'
         i += 1
 
-    return 0
+    return where_query
+
+
+def groupby_clause(process_metadata):
+    process_metadata = process_metadata.fillna('')
+    if process_metadata['agg_flg'].str.contains('N').any():
+        dim_columns = process_metadata[['column_source', 'column_name', 'column_custom_flg', 'agg_flg',
+                                        'agg_operation']].drop_duplicates().reset_index(drop=True)
+
+        dim_tables_filtered = dim_columns.loc[(dim_columns['agg_flg'] == 'Y') & (dim_columns['column_name'] != '')]\
+            .reset_index(drop=True)
+        cant_columnas_group = len(dim_tables_filtered)
+        groupby_query = (''.join(['group by', '\n']))
+
+        i = 1
+
+        for index, row in dim_tables_filtered.iterrows():
+
+            if row['column_custom_flg'] == 'N':
+                groupby_query += (''.join([row['column_source'], '.', row['column_name']]))
+            else:
+                groupby_query += (''.join([row['column_name']]))
+
+            if i < cant_columnas_group:
+                groupby_query += (''.join([',', '\n']))
+            else:
+                groupby_query += ';\n'
+
+            i += 1
+    else:
+        groupby_query = ''
+    return groupby_query
+
+
+def handler_sq(process_metadata):
+
+    df_sq = process_metadata['subquery_name'].drop_duplicates().dropna().reset_index(drop=True)
+
+    lista_sq = df_sq.values.tolist()
+
+    dict_sq = {}
+
+    i = 0
+    if len(lista_sq) != 0:
+        for item in lista_sq:
+
+            process_metadata = process_metadata.loc[(process_metadata['subquery_name'] == lista_sq[i])]
+            # SELECT
+
+            subquery = select_clause(process_metadata)
+
+            # FROM
+            process_metadata_from = process_metadata.loc[(process_metadata['table_a'].notnull())].reset_index(drop=True)
+            subquery += (''.join([' ', from_clause(process_metadata_from, dict_sq)]))
+
+            # WHERE
+            process_metadata_where = process_metadata.loc[(process_metadata['where_value_a'].notnull()
+                                                         | process_metadata['where_value_b'].notnull())]\
+                .reset_index(drop=True)
+            subquery += (''.join([' ', where_clause(process_metadata_where, dict_sq)]))
+
+            # GROUP BY
+            subquery += groupby_clause(process_metadata)
+
+            dict_sq[lista_sq[i]] = subquery
+
+            i += 1
+    return dict_sq
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 import sys
-from bin.functions import groupby_clause, where_clause, select_clause, from_clause, header, parse_arguments
+from bin.functions import groupby_clause, where_clause, select_clause, from_clause, header, parse_arguments, handler_sq
 
 # Sample Arguments #
 #arg = ['-p', 'trafico_actuaciones_f_tr_actuacion_detallada',
@@ -14,9 +14,9 @@ from bin.functions import groupby_clause, where_clause, select_clause, from_clau
 
 process_name, chdir, file_name, output_file = parse_arguments(sys.argv[1:])
 
-#process_name = 'test'
+#process_name = 'tc_titulares'
 #chdir = 'C:\\Users\\Jonathan Boianover\\Desktop\\'
-#file_name = 'process_metadata_galicia.xls'
+#file_name = 'process_metadata_reglas.xls'
 #output_file = 'groupby_output.txt'
 
 pm = pd.read_excel(os.path.join(chdir, file_name), sheet_name=0, header=0, names=None, index_col=None,
@@ -24,32 +24,43 @@ pm = pd.read_excel(os.path.join(chdir, file_name), sheet_name=0, header=0, names
 
 file = open(os.path.join(chdir, output_file), 'w', encoding='utf8')
 
-pm = pm.loc[(pm['process_name'] == process_name) & (pm['active_flg'] == 'Y')]
+pm = pm.loc[(pm['indicador'] == process_name) & (pm['active_flg'] == 'Y')]
 
-batches = pm['batch'].drop_duplicates().astype(int).tolist()
+indicadores = pm['indicador'].drop_duplicates().astype(str).tolist()
 
+for indicador in indicadores:
 
-for batch in batches:
-
-    process_metadata = pm.loc[(pm['batch'] == batch)]
+    process_metadata = pm.loc[(pm['indicador'] == indicador)]
     process_table = process_metadata['dest_table'].drop_duplicates().dropna().reset_index(drop=True)
     process_table = process_table[0]
 
+    if process_metadata['sq_flg'].str.contains('Y').any():
+        subquery_df = process_metadata.loc[process_metadata['sq_flg'] == 'Y']
+        dict_sq = handler_sq(subquery_df)
+    else:
+        dict_sq = {}
+
     # HEADER
-    header(file, process_table)
+    header_query = header(process_table)
 
     # SELECT
-    select_clause(file, process_metadata)
+    process_metadata_query = process_metadata.loc[process_metadata['sq_flg'] != 'Y'].reset_index(drop=True)
+    select_query = select_clause(process_metadata_query)
 
     # FROM
-    from_clause(file, process_metadata)
+    from_query = from_clause(process_metadata_query, dict_sq)
 
     # WHERE
-    where_clause(file, process_metadata)
+    process_metadata_where = process_metadata_query.loc[(process_metadata_query['where_value_a'].notnull()
+                                                         | process_metadata_query['where_value_b'].notnull())]\
+        .reset_index(drop=True)
+    where_query = where_clause(process_metadata_where, dict_sq)
 
     # GROUP
-    groupby_clause(file, process_metadata)
+    groupby_query = groupby_clause(process_metadata_query)
 
-    file.writelines('---------\n\n')
+    sql_final = ''.join([select_query, '\n', from_query, where_query, '\n', groupby_query, '---------------------\n\n'])
+
+    file.writelines(sql_final)
 
 file.close()
